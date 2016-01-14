e3ed36056e058e7811aea7e6d21c7f2418b2744b
JessicaHerford <jrherford12@gmail.com>
2016-01-14 15:47:05 -0700

    API Call Working

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+var unirest = require('unirest');
+var api_key= process.env.NYT_API_KEY;
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Server Side API Calls' });
 });
 
+
 router.get('/books/index', function(req, res, next) {
   res.render('books/index');
 });
+
+router.get('/books', function(req, res) {
+    unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + api_key)
+    .end(function (response) {
+        var NYTBooks = response.body.results.books;
+        console.log(NYTBooks);
+        res.render('books/index', {books: NYTBooks});
+      })
+})
+
+
 
 router.get('/books/show', function(req, res, next) {
   res.render('books/show');
